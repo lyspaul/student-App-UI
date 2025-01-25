5cb450168982b62564926dbb5a32ce24867a9cda
Lys Paul Lukuka <lyslukuka@gmail.com>
2025-01-24 18:35:49 -0600

    Added the Search component to search a student by first name, and implemented that in the StudentsView.js component
    Application is fully functional

--- src/Component/student/StudentsView.js
+++ src/Component/student/StudentsView.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { FaEdit, FaEye, FaTrashAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Search from "../Common/Search";
 
 const StudentsView = () => {
   //students array to hold all students from the back end and it includes the setStudents method.
   const [students, setStudents] = useState([]);
+
+  //search string variable value initialized to empty
+  const [search, setSearch] = useState("");
 
   //Display the students data on the UI.
   useEffect(() => {
@@ -23,13 +27,15 @@ const StudentsView = () => {
       setStudents(result.data);
     }
   };
-
+  //variable that deletes a student profile based on student id
   const handleDelete = async (id) => {
     await axios.delete(`http://localhost:9192/students/delete/${id}`);
     loadStudents();
   };
   return (
     <section>
+      {/* Initialize to set search as a function */}
+      <Search search={search} setSearch={setSearch} />
       <table className="table table-bordered table-hover shadow">
         <thead>
           <tr className="text-center">
@@ -43,41 +49,43 @@ const StudentsView = () => {
         </thead>
 
         <tbody className="text-center">
-          {students.map((student, index) => (
-            <tr key={student.id}>
-              <th scope="row" key={index}>
-                {index + 1}
-              </th>
-              <td>{student.firstName}</td>
-              <td>{student.lastName}</td>
-              <td>{student.email}</td>
-              <td>{student.department}</td>
-              <td className="mx-2">
-                <Link
-                  to={`/student-profile/${student.id}`}
-                  className="btn btn-info"
-                >
-                  <FaEye />
-                </Link>
-              </td>
-              <td className="mx-2">
-                <Link
-                  to={`/update-student/${student.id}`}
-                  className="btn btn-warning"
-                >
-                  <FaEdit />
-                </Link>
-              </td>
-              <td className="mx-2">
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(student.id)}
-                >
-                  <FaTrashAlt />
-                </button>
-              </td>
-            </tr>
-          ))}
+          {students
+            .filter((st) => st.firstName.toLowerCase().includes(search))
+            .map((student, index) => (
+              <tr key={student.id}>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{student.firstName}</td>
+                <td>{student.lastName}</td>
+                <td>{student.email}</td>
+                <td>{student.department}</td>
+                <td className="mx-2">
+                  <Link
+                    to={`/student-profile/${student.id}`}
+                    className="btn btn-info"
+                  >
+                    <FaEye />
+                  </Link>
+                </td>
+                <td className="mx-2">
+                  <Link
+                    to={`/update-student/${student.id}`}
+                    className="btn btn-warning"
+                  >
+                    <FaEdit />
+                  </Link>
+                </td>
+                <td className="mx-2">
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(student.id)}
+                  >
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </section>
